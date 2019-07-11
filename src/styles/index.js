1d318c3ff131ca12468564a13e83a8d3d7d8ead0
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-07-11 16:40:18 +0100

    style navigation

--- src/styles/index.js
+++ src/styles/index.js
@@ -4,10 +4,12 @@ const Navigation = styled.nav`
   color: white;
   /* background-color: #000b0f; */
   opacity: 1;
-
+  border-bottom: 0.1px solid #8c8c8c;
+  margin: 0;
+  padding: 1rem 0;
   img {
     width: 100px;
-    height: 100px;
+    height: auto;
     margin-left: 3rem;
     cursor: pointer;
   }
