4e65fa97d96d3d6dd9441110116b4874ff8ca91d
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-07-11 11:27:50 +0100

    add contact markup

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react"
+
+import { Contact as StyledContact } from "../../styles"
 
 export default function Contact() {
-    return (
-        <div>
-            <h3>Contact Me</h3>
+  return (
+    <StyledContact>
+      <h3>Contact Me</h3>
 
-            <input type="text" placeholder="Your Email" />
-            <input type="text" placeholder="ekunolaeasybuoy@gmail.com" />
-            <button>Send</button>
-        </div>
-    )
+      <input type="text" placeholder="Your Email" />
+      <input type="text" placeholder="ekunolaeasybuoy@gmail.com" />
+      <button>Send</button>
+    </StyledContact>
+  )
 }
