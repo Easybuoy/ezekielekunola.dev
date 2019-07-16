ef6e95ecf2bf113210fb448b648c3b3749c77799
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-07-16 02:55:17 +0100

    fix bug

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -13,31 +13,37 @@ export default function Header() {
       <div className="header-content">
         <div className="contact-details">
           <a href="mailto:ekunolaeasybuoy@gmail.com">
-            <i className="fas fa-envelope fa-5x"></i>
+            <i className="fas fa-envelope fa-5x" />
           </a>
 
           <a href="https://github.com/easybuoy">
-            <i className="fab fa-github fa-5x"></i>
+            <i className="fab fa-github fa-5x" />
           </a>
 
           <a href="https://www.linkedin.com/in/easybuoy/">
-            <i className="fab fa-linkedin fa-5x"></i>
+            <i className="fab fa-linkedin fa-5x" />
           </a>
 
           <a href="https://dev.to/easybuoy">
-            <i className="fab fa-dev fa-5x"></i>
+            <i className="fab fa-dev fa-5x" />
           </a>
 
           <a href="https://medium.com/@easybuoy">
-            <i className="fab fa-medium fa-5x"></i>
+            <i className="fab fa-medium fa-5x" />
           </a>
 
           <a href="https://twitter.com/easybuoy">
-            <i className="fab fa-twitter fa-5x"></i>
+            <i className="fab fa-twitter fa-5x" />
           </a>
         </div>
         <div className="header-text">
-          <h1>Hello <span role="img" aria-label="Wave">👋🏾</span>, I'm Ezekiel</h1>
+          <h1>
+            Hello{" "}
+            <span role="img" aria-label="Wave">
+              👋🏾
+            </span>
+            , I'm Ezekiel
+          </h1>{" "}
           <Typist
             cursor={{
               show: true,
@@ -57,12 +63,10 @@ export default function Header() {
             <Typist.Backspace count={16} delay={500} />
             <span>Technical Writer</span>
           </Typist>
-
           <div className="header-actions">
-            <a href={Resume} target="_blank" rel="noopener noreferrer">
+            {/* <a href={Resume} target="_blank" rel="noopener noreferrer">
               <Button>View Resume</Button>
-            </a>
-            {/* <Button>My Work</Button> */}
+            </a> */}
           </div>
         </div>
       </div>
