889cd15c9810dfd55d7c220f3f0374cbb3a7c263
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-07-16 11:32:30 +0100

    refactor

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,15 +8,15 @@ export default function Contact() {
     <StyledContact>
       <h3>Contact Me</h3>
       {/* action="https://formspree.io/ekunolaeasybuoy@gmail.com" method="POST" */}
-      <Form >
+      <Form action="https://formspree.io/ekunolaeasybuoy@gmail.com" method="POST">
         <label htmlFor="name">Your Name</label>
-        <Input type="text" id="name" placeholder="Your Name" required />
+        <Input type="text" id="name" placeholder="Your Name" name="name" required />
 
         <label htmlFor="email">Your Email</label>
-        <Input type="email" id="email" placeholder="Your Email" required />
+        <Input type="email" id="email" placeholder="Your Email" name="email" required />
 
         <label htmlFor="message">Message</label>
-        <TextArea id="message" placeholder="Your message" required/>
+        <TextArea id="message" placeholder="Your message" name="message" required/>
         <Button>Send</Button>
       </Form>
     </StyledContact>
