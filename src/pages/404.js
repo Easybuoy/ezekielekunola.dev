e255751cea5aee59f1dc3aaa0f1e1c75b5f2c8d3
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-10-20 17:19:27 +0100

    create error page

---
+++ src/pages/404.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/Layout"
+import errorImage from "../images/error.svg"
+import { Success as StyledSuccess } from "../styles"
+import PatternImage from "../images/pattern.png"
+import Navigation from "../components/Common/Navigation"
+
+const Success = props => (
+  <Layout>
+    <Navigation />
+    <StyledSuccess>
+      <div
+        className="success"
+        style={{
+          backgroundImage: `linear-gradient(to right bottom, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0)), url(${PatternImage})`,
+        }}
+      >
+        <div className="success-image">
+          <img src={errorImage} alt="" />
+        </div>
+
+        <div className="success-content">
+          <div className="success-content-detail">
+            <h2>Ooops!!!    Page not found</h2>
+            <Link to="/">&larr; Back Home</Link>
+          </div>
+        </div>
+      </div>
+    </StyledSuccess>
+  </Layout>
+)
+
+export default Success
