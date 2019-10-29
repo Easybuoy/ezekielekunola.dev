73a3e58a648e7c7fdeb751322cb95cc9316193ff
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-10-29 17:24:38 +0100

    style skills section

--- src/components/Skills/SkillsList.jsx
+++ src/components/Skills/SkillsList.jsx
@@ -35,8 +35,22 @@ const SkillsList = () => {
         </div>
 
         <div className="skills-body">
-          <div className="skills-body-content"></div>
-          <div className="skills-body-items"></div>
+          <div className="skills-body-content">
+            <p>
+              As someone who loves the art of software development, I have a
+              passion for writing robust, clean, and maintainable code. This
+              also means I strive to follow best practices and use
+              industry-standard tools, including git-flow, TDD, linting, package
+              management (Yarn, NPM, and Pipenv), and modern build tools
+              (Webpack and Parcel). Although I've a wide range of experience,
+              the technology I prefer and/or am most familiar with includes
+              Python, Django, PostgreSQL, React.js, JavaScript ES6,
+              Sass/CSS/PostCSS, HTML, Twitter Bootstrap, and Linux.
+            </p>
+          </div>
+          <div className="skills-body-items">
+            
+          </div>
         </div>
 
         {/* {skills.map(({ node: skill }) => {
